1bd3406407917ffc3fbb5c8408909e29ac1666f0
Big198801 <bigniko@mail.ru>
2022-11-29 00:04:02 +0300

    wip, ex003, basic termins

--- EX002/Program.cs
+++ EX002/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("hello, input your name:");
 
-var name =  Console.ReadLine();
+string name =  Console.ReadLine();
 Console.WriteLine("hello,  " + name);
 
 Console.WriteLine("then, bye bye,  " + name);
