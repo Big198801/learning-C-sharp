c92207f5b9a14eec552e9a909797ac08c3ae52e4
Big198801 <bigniko@mail.ru>
2022-11-30 14:09:27 +0300

    done lerning EX005 : new Random().Next(min, max-1)

---
+++ EX005/Program.cs
@@ -0,0 +1,12 @@
+﻿//пример с использованием  new Random().Next(min, max-1)
+
+int number1 = new Random().Next(1, 9);
+Console.WriteLine(number1);
+int number2 = new Random().Next(1, 9);
+Console.WriteLine(number2);
+int numbersSum = number1 + number2;
+
+Console.Write("сумма этих часел равна: ");
+Console.WriteLine(numbersSum);
+
+Console.WriteLine("программа завершена");
