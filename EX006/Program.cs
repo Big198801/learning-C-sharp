e579f67fabea1aab03be3a528043c3403be95155
Big198801 <bigniko@mail.ru>
2022-11-30 16:24:57 +0300

    ex006 done

---
+++ EX006/Program.cs
@@ -0,0 +1,15 @@
+﻿//особое приветстие при выполнении условия 
+
+Console.Write("input your name:  ");
+
+string nameInput = Console.ReadLine();
+
+if(nameInput.ToLower() == "masha")
+{
+    Console.WriteLine("thanks GOD! This is you!!");
+}
+
+else
+{
+    Console.WriteLine(("ну привеет,  ") + (nameInput));
+}
