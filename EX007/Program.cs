0aeb70def17aa6650b10ecb1705697e5a0624483
Big198801 <bigniko@mail.ru>
2022-11-30 17:04:21 +0300

    done ex007/ find max number

---
+++ EX007/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+int a = 3;
+int b = 6;
+int c = 3;
+int d = 5;
+int e = 10;
+int f = 6;
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+if(f > max) max = f;
+
+
+
+Console.Write("максимальная переменая была ");
+Console.WriteLine(max);
