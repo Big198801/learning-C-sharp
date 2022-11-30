99ca2cf718cdf17cac67df45ecfdf6a2f609d45d
Big198801 <bigniko@mail.ru>
2022-11-30 05:29:16 +0300

    minor fix in second folder

---
+++ Second/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("введите число");
+string numberStr = Console.ReadLine() ??"";
+int number = Convert.ToInt32(numberStr);
+int result  = number  numberStr;
+Console.WriteLine($"квадрат числа {number} = {result}");
