50e3950135e1c1903ac4199c902281a292a868db
Big198801 <bigniko@mail.ru>
2022-12-01 08:08:39 +0300

    added exercises after lecture 2

---
+++ EX008_Methods/Program.cs
@@ -0,0 +1,15 @@
+﻿int max(int)
+int a = 3;
+int b = 6;
+int c = 3;
+int d = 5;
+int e = 10;
+int f = 6;
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+if(f > max) max = f;
---
+++ EX010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1,3,4,799,6,7,799,5};
+
+int n = array.Length;
+int find = 799;
+int index = 0;
+
+while (index < n)
+{
+if (array[index] == find)
+  {
+    Console.WriteLine(index);
+    break;
+    index++ ;
+  }
+else
+   index++ ;
+}
