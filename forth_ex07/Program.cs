5faaf33dfdef16fe17e484cdb0435ad7fc1310ce
Big198801 <bigniko@mail.ru>
2022-12-01 22:14:18 +0300

    all changes applied

---
+++ forth_ex07/Program.cs
@@ -0,0 +1,28 @@
+﻿//задача 7: на входе принимаем трехначное число, а на выходе выводим последнюю цифру того числа
+
+Console.WriteLine("Как тебя зовут?");
+string puppy = Console.ReadLine()??"";
+Console.WriteLine($"привет ,  {puppy}");
+Console.WriteLine($"для тебя есть задача {puppy}");
+Console.WriteLine("Итак, на входе принимаем трехначное число, а на выходе выводим последнюю цифру того числа");
+Console.WriteLine($"ты справишся, {puppy} ?");
+string answr = Console.ReadLine() ??"";
+
+if (answr.ToLower() == "да")
+{
+    Console.WriteLine("let's gooooo");
+}
+else   
+Console.WriteLine("досвидания(шучу, все равно решаем, деваться некуда)))) ");
+    
+Console.WriteLine("ВВЕДИ ЧИСЛО");
+
+
+string chislo = Console.ReadLine() ??"";
+
+
+int chislo_int = Convert.ToInt32(chislo);
+
+Console.WriteLine(chislo_int % 10);
+
+
---
+++ third_ex5/Program.cs
@@ -0,0 +1,9 @@
+﻿// задание5 : на вход n , а на выходе число от -n до n
+Console.Write("input any namber: ");
+string name1 = Console.ReadLine()?? "";
+int in_name = Convert.ToInt32(name1);
+in_name = Math.Abs(in_name);
+for (int i = (-in_name); i <=in_name; i++)
+{
+    Console.Write($"{i}  ");
+}
