982743cb9a83246f7399a781c43a0e75493015a9
Big198801 <bigniko@mail.ru>
2022-12-02 00:32:29 +0300

    EX08 done. homework is complete

---
+++ HW_C#/EX008/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+Console.Clear();
+Console.WriteLine("ВВедите число: ");
+string nn = Console.ReadLine() ??"";
+Int64 n = Convert.ToInt64(nn);
+//n = Math.Abs(n);
+
+if (n > 0)
+{
+for (int i = 1; i <= n; i++)
+{
+   if ((i % 2) ==0 )
+   {
+    Console.Write($"{i} ");
+   } 
+  
+}}
+if (n == 0)
+{
+    Console.WriteLine("число было = 0 /программа завешена");
+}
+
+else
+Console.WriteLine("было введено отрицательное число");
+ for (int i = 1; i >= n; i--)
+{
+   if ((i % 2) ==0 )
+   {
+    Console.Write($"{i} ");
+   }    
+   
+}
+
+Console.WriteLine("программа завешена");
+
+
+
+
+
+
