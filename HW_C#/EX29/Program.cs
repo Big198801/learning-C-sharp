8c38532263433217bf3c468c1f2c5b19852d6fe5
Big198801 <bigniko@mail.ru>
2022-12-12 22:42:15 +0300

    ex29 done

---
+++ HW_C#/EX29/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 29: Напишите программу, 
+которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+
+void Fill_array(int[] arr)
+{
+int lengh = arr.Length;
+int ind = 0;
+Console.Write("[ ");
+while (ind < lengh)
+{
+  arr[ind] = new Random().Next(1, 100); 
+  Console.Write($"{arr[ind]} ");
+  ind++; 
+}
+Console.Write("]");
+Console.WriteLine();
+}
+
+int[] array = new int[8];
+Fill_array(array);
+
+
