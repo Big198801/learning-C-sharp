3fe022c02603fb60313d89b4fe16a9bc191d6b4f
Big198801 <bigniko@mail.ru>
2022-12-18 15:58:11 +0300

    Lesson6 repeated

---
+++ Lesson6/EX42/Program.cs
@@ -0,0 +1,64 @@
+﻿// Создайте программу, преобразующее десятичное число в двоичное
+// 45 => 101101
+// 3 => 11
+// 2 => 10
+
+
+// сгенерируем число
+
+int GetNumber(string message)
+{
+    bool iscorrect = false;
+    int result = 0;
+    while (!iscorrect)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            iscorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("ввели не число, повторите ввод");
+        }
+    }
+    return result;
+
+}
+//программа, которая делит полученное число с остатком на 2
+string Double(int number)
+{
+    int ostatok = number;
+    string result = string.Empty;
+    while (ostatok > 0)
+    {
+
+        result = result + Convert.ToString(ostatok % 2);
+        ostatok = ostatok / 2;
+    }
+    
+
+    return new string (result.Reverse().ToArray());
+}
+
+
+
+
+
+
+
+
+int decimal_number = GetNumber("Введите число");
+Console.WriteLine($" преобразуем {decimal_number} в двоичное...");
+
+string double_num = Double(decimal_number);
+Console.WriteLine(double_num);
+
+/*int[] res_array = new int[result.Length];
+int count = 0;
+for (int i = result.Length - 1; i <= 0; i--)
+{
+    res_array[count] = Convert.ToInt32(result[i].ToString());
+    Console.Write(res_array[count]);
+}
+*/
--- Lesson6/EX44/Program.cs
+++ Lesson6/EX44/Program.cs
@@ -1,1 +1,49 @@
-﻿
+﻿// не используя рекурсию, выведите первые N цифр фибонччи
+
+
+int GetNumber(string Message)
+{
+    bool iscorrect = false;
+    int number = 0;
+
+    while (!iscorrect)
+    {
+        Console.WriteLine(Message);
+        if (int.TryParse(Console.ReadLine(), out number) && number > 0)
+        {
+            iscorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("ввели не число...");
+        }
+
+    }
+    return number;
+}
+
+
+
+int Fib_to_N(int number)
+{
+    int N = number;
+    int num0 = 0;
+    int num1 = 1;
+    int sum = 0;
+    Console.Write("0");
+   for (int i = 1; i < N; i++)
+   {
+    sum = num0 + num1;
+    num0 = num1;
+    num1 = sum;
+    Console.Write($" {sum}");
+
+   }
+    
+    return sum;
+}
+
+int N_fib = GetNumber("Введите число");
+Console.WriteLine($"получено число {N_fib} ");
+
+int fib_num = Fib_to_N(N_fib);
