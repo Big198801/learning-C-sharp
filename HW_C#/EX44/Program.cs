670d7993cdfc1c1d0a4229de9f01d0dab2a21eb6
Big198801 <bigniko@mail.ru>
2022-12-21 00:55:57 +0300

    HW EX44 - done

---
+++ HW_C#/EX44/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+//1. метод для получения числа
+double GetNumber(string message)
+{
+    double result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (double.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
+
+// метод для находжения точки пересечения
+(double, double) GetCrossPodouble(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    return (x, y);
+}
+double b1 = GetNumber("inpit b1: ");
+double k1 = GetNumber("input k1: ");
+double b2 = GetNumber("input b2: ");
+double k2 = GetNumber("input k2: ");
+
+double x;
+double y;
+Console.WriteLine($"Точка пересечения {(x, y) = GetCrossPodouble(b1, k1, b2, k2)}");
