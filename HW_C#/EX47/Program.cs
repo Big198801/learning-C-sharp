f85c49a5637396289c31cc97137453436fc6f8b4
Big198801 <bigniko@mail.ru>
2022-12-23 00:42:49 +0300

    EX47 and EX50 is DONE

---
+++ HW_C#/EX47/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+
+1 -3,3 8 -9,9
+
+8 7,8 -7,1 9   */
+
+
+//1. генерируем массив.
+double[,] GetArray(int rows, int columns)
+{
+    double[,] array = new double[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = rnd.Next(-10, 100) + Math.Round((rnd.NextDouble()), 1);
+        }
+    }
+    return array;
+}
+
+
+// 2. распечатываем массив
+void PrintDoubleArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+     for (int j = 0; j < array.GetLength(1); j++)
+     {
+        Console.Write($"{array[i,j]} ");
+     }
+     Console.WriteLine();   
+    }
+}
+
+// 3. Решаем поставленную задачу.
+double[,] double_array = GetArray(3, 4);
+PrintDoubleArray(double_array);
+
---
+++ HW_C#/EX50/Program.cs
@@ -0,0 +1,98 @@
+﻿/* Задача 50. Напишите программу, которая на вход принимает
+позиции элемента в двумерном массиве, и возвращает значение
+этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+17 -> такого числа в массиве нет */
+
+// 1. задаем массив
+int[,] arrayDouble = new int[,]
+{{1, 4, 7, 2},
+{5, 9, 2, 3},
+{8, 4, 2, 4}};
+
+// 2. принимаем значение позиции элемента по строке
+int GetRowNumber(string message)
+{
+    Console.WriteLine(message);
+    int result;
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result) && result >= 0 && result < 3)
+            break;
+        else
+        {
+            Console.WriteLine("ввели не число, либо неверный диапазон(0-2)");
+        }
+    }
+    return result;
+}
+
+// 2. Принимаем значение позиции элемента по столбцу
+int GetColumnNumber(string message)
+{
+    Console.WriteLine(message);
+    int result;
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result) && result >= 0 && result < 4)
+            break;
+        else
+        {
+            Console.WriteLine("ввели не число, либо неверный диапазон(0-3)");
+        }
+    }
+    return result;
+}
+
+// 3. Запрашиваем число, которое желаем найти в двумерном массиве
+int GetFindNumber(string message)
+{
+    int result;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result) && result >= 0 && result < 20)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Выберите элемент от 0 до 9");
+        }
+    }
+    return result;
+}
+
+int row = GetRowNumber("выбор строки от 0 до 2:");
+int columns = GetColumnNumber("ввыбор столбца от 0 до 3:");
+Console.WriteLine($"это адрес числа {arrayDouble[row, columns]}");
+Console.WriteLine("_____________________________");
+int isThisNumber = GetFindNumber("введите число, которое хотите найти:");
+Console.WriteLine("_____________________________");
+// 4. Проверяем наличие указанного числа в Массиве
+bool isCorrect = false;
+while (!isCorrect)
+{
+    for (int i = 0; i < arrayDouble.GetLength(0); i++)
+    {
+        if (isThisNumber > 10)
+        {
+            Console.WriteLine($"{isThisNumber} -> такого числа в массиве нет");
+            isCorrect = true;
+            break;
+        }
+        for (int j = 0; j < arrayDouble.GetLength(1); j++)
+        {
+            if (arrayDouble[i, j] == isThisNumber)
+            {
+                Console.WriteLine($"число {isThisNumber} находилось на позициях строки {i + 1}  столбца {j + 1}.");
+                isCorrect = true;
+            }
+        }
+    }
+}
